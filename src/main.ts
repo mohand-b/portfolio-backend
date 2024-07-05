255d69b60dfcecbd6cba60276e7130a3123c7eb6
mohand-b <mohandbdj@gmail.com>
2024-07-05 13:24:21 +0200

    fix: ensure the application listens on the PORT provided by Heroku

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap().then(() => console.log('Server is running!'));
